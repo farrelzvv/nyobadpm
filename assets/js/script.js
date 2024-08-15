5cfc05d736ab72d9f5d2f97706376bda682e39bb
farrelzvv <fzulviano@gmail.com>
2024-08-15 20:57:59 +0700

    Nyoba dpm

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+navToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const dropdownBtn = document.querySelector('.dropdown-btn');
+  const subNavbar = document.querySelector('.sub-navbar');
+
+  dropdownBtn.addEventListener('click', function() {
+    subNavbar.classList.toggle('active');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1, // Menampilkan satu slide (dengan tiga card) dalam satu waktu
+  });
+});
